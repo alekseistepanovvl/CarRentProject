2e39771ebab832094ecbbe956d7e3cd445572213
alekseistepanovvl <alekseistepanovvl@gmail.com>
2024-05-05 23:29:37 +0200

    migrate to modularity

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module CarRentProject {
+    requires lombok;
+    requires spring.context;
+    requires spring.tx;
+    requires spring.data.jpa;
+    requires java.persistence;
+    requires org.mapstruct;
+    requires io.swagger.v3.oas.annotations;
+    requires org.hibernate.orm.core;
+    requires java.validation;
+    requires liquibase.core;
+    requires java.sql;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires spring.data.commons;
+    requires spring.web;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires com.zaxxer.hikari;
+    requires spring.beans;
+}
